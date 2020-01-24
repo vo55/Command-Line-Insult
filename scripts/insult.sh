93400609bd74ebf00fd3d9326b1f56e40118fadb
philipbarwi <philip.barwi@web.de>
2020-01-25 00:56:17 +0100

    add some color

--- scripts/insult.sh
+++ scripts/insult.sh
@@ -2,7 +2,7 @@
 
 do_insult () {
  insult=$(curl -s https://evilinsult.com/generate_insult.php?lang=en&type=tex)
- printf '%s\n' "$insult" 
+ printf '\n\e[96m%s\e[0m\n' "$insult"
 }
 git "$@"
 if (( RANDOM % 2 )); then do_insult;fi;
